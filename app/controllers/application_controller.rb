f7f0f3cae5f0fdc2922ae02f5e799ffaa14da62a
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2025-03-08 21:24:27 +0100

    Displaying as grid/list depending on the preference

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,24 +2,23 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  VALID_DISPLAY_MODES = %w[ grid list ]
-
   before_action :set_display_mode
 
   private
 
+  VALID_DISPLAY_MODES = %w[ grid list ]
+  DEFAULT_DISPLAY_MODE = "grid"
+
   def current_display_mode
-    params[:display_mode] || cookies[:display_mode] || "grid"
+    params[:display_mode] || cookies[:display_mode] || DEFAULT_DISPLAY_MODE
   end
   helper_method :current_display_mode
 
   def set_display_mode
-    display_mode = current_display_mode
-
-    cookies[:display_mode] = if VALID_DISPLAY_MODES.include?(display_mode)
-      display_mode
+    cookies[:display_mode] = if VALID_DISPLAY_MODES.include?(current_display_mode)
+      current_display_mode
     else
-      "grid"
+      DEFAULT_DISPLAY_MODE
     end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_grid?
+    current_display_mode == "grid"
+  end
+
+  def display_list?
+    !display_grid?
+  end
 end
