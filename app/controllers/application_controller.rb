bc2d5b67c084c0df8c14ad831313ed4fb26b9e63
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2025-03-08 21:00:40 +0100

    No need to handle different viewports for this

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  VALID_DISPLAY_MODES = %w[ grid list ]
+
+  before_action :set_display_mode
+
+  private
+
+  def set_display_mode
+    display_mode = params[:display_mode] || cookies[:display_mode] || "grid"
+
+    cookies[:display_mode] = if VALID_DISPLAY_MODES.include?(display_mode)
+      display_mode
+    else
+      "grid"
+    end
+  end
 end
