d15dac17e0402cf49050001e7280ee8b980602d8
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2025-03-08 21:02:16 +0100

    Added a test to make sure display mode is configurable

---
+++ test/integration/display_mode_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class DisplayModeTest < ActionDispatch::IntegrationTest
+  test "display mode is set to grid by default" do
+    get projects_path
+    assert_equal "grid", cookies[:display_mode]
+  end
+
+  test "display mode is set to list" do
+    get projects_path(display_mode: "list")
+
+    assert_equal "list", cookies[:display_mode]
+  end
+end
