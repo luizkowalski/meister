d955fccd985888e624022dcf8eb31f8e17abea10
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2025-03-09 01:16:45 +0100

    Realized I don't need to broadcast a refresh here, the model does it

--- app/jobs/task_seeder_job.rb
+++ app/jobs/task_seeder_job.rb
@@ -5,7 +5,6 @@ class TaskSeederJob < ApplicationJob
   def perform
     Project.find_each do |project|
       project.tasks.create!(name: "New Task #{Time.current.strftime('%d-%m-%Y %H:%M:%S')}")
-      broadcast_refresh_to project
     end
   end
 end
