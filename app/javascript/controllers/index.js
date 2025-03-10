85adc6ef262138ce2dd7eec6730a28ae9686e807
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2025-03-10 11:53:45 +0100

    Adding view transitions for smooth page changes and add classes so screen doesn't flick

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,12 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+addEventListener('turbo:before-frame-render', (event) => {
+  if (document.startViewTransition) {
+    const originalRender = event.detail.render
+    event.detail.render = (currentElement, newElement) => {
+      document.startViewTransition(() => originalRender(currentElement, newElement))
+    }
+  }
+})
